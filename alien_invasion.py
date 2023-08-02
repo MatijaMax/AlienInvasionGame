c96da8f5365bbbdc8dc835d59f416df755d08d9e
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2023-08-02 21:24:34 +0200

    mini refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,20 +27,23 @@ class AlienInvasion:
         #start the main loop of the game
         while True:
             #keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
 
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
             #redraw screen color
-            self.screen.fill(self.bg_color)
-            
+            self.screen.fill(self.bg_color)          
             #redraw screen color bg
             self.screen.blit(self.bg_image, (0, 0))
             self.ship.blitme()
-
             #most recent screen visibility
             pygame.display.flip()
-            self.clock.tick(60)
 
 
 if __name__ == '__main__':
